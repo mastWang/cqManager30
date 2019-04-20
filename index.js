e35ddb9b712acb86fec08a4067d610b4968c7592
autumnfish <517729329@qq.com>
2019-04-20 11:28:48 +0800

    项目准备工作

---
+++ index.js
@@ -0,0 +1,11 @@
+// 导入模块
+const express = require('express')
+const dbHelper = require('./libs/dbHelper')
+
+// 实例化服务器对象
+const app = express()
+// 托管静态资源
+app.use(express.static('views'))
+
+// 开启监听
+app.listen(8848)
